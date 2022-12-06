1159a84faa336e17f6664af033fb392731f0cde9
Maria-Beatriz18 <mmbbeatriz.15@gmail.com>
2022-12-05 21:04:19 -0300

    Aula04Node

--- app.js
+++ app.js
@@ -9,42 +9,86 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 //Handling GET request
-app.get('/', (req,res)=> {
-    res.send('Hello World')
-    res.end()
-})
+
+//app.get('/', (req, res) => {
+//res.send('A simple Node App is' + 'running on this server')
+//res.end()
+//})
+
+app.get('/', function (req, res, next) {
+    console.log('A API está rodando' + 'nesse servidor');
+    next();
+  }, function (req, res) {
+    res.send('Hello World!');
+    res.end();
+  });
 
 
+//ESTRUTURA DE UMA API DE CONSULTA DE DADOS.
 app.get('/clientes', (req, res) =>{
-res.setHeader("Access-Control-Allow-Origin","*");
-res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-return connect.execSQLQuery('select * from cliente', res);
-})
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    return connect.execSQLQuery('select * from cliente', res);
+    })
+
+//app.get('/clientes', (req, res) =>{
+//return connect.execSQLQuery('select * from cliente', res);
+//})
 
 
+//ESTRUTURA DE UMA API DE CONSULTA DE DADOS COM ID.
 app.get('/clientes/:id', (req, res) =>{
-res.setHeader("Access-Control-Allow-Origin","*");
-res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-return connect.execSQLQuery('select * from cliente where id='+ req.params.id, res);
-})
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    return connect.execSQLQuery('select * from cliente where id='+ req.params.id, res);
+    })
+
+//app.get('/clientes/:id?', (req, res) => {
+//    let filter = '';
+//    if(req.params.id) filter = ' WHERE ID=' + parseInt(req.params.id);
+//    return connect.execSQLQuery('select * from cliente' + filter, res);
+//})
+
+
+//MÉTODO PUT - ALTERAR DADOS
 
 app.put('/clientes/:id', (req, res) =>{
-res.setHeader("Access-Control-Allow-Origin","*");
-res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-return connect.execSQLQuery("update cliente set nome='"+req.body.nome+"' where id="+req.params.id, res);
-})
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    return connect.execSQLQuery("update cliente set nome='"+req.body.nome+"' where id="+req.params.id, res);
+    })
+
+
+//app.put('/clientes/:id', (req, res) => {
+//   const id = parseInt(req.params.id);
+//    const nome = req.body.nome.substring(0,100);
+//    return connect.execSQLQuery(`update cliente set nome='${nome}' WHERE ID=${id}`, res);
+//})
+
+
+//MÉTODO POST - INCLUIR DADOS
 
 app.post('/clientes/', (req, res) =>{
-res.setHeader("Access-Control-Allow-Origin","*");
-res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-return connect.execSQLQuery("insert into cliente (nome) value('"+req.body.nome+"')", res);
-})
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    return connect.execSQLQuery("insert into cliente (nome) value('"+req.body.nome+"')", res);
+    })
+    
+    
+//app.post('/clientes/', (req, res) =>{
+//return connect.execSQLQuery( "insert into cliente (nome) value('"+req.body.nome+"')", res);
+//})
 
+//MÉTODO DELETE - DELETAR DADOS
 app.delete('/clientes/:id', (req, res) =>{
-res.setHeader("Access-Control-Allow-Origin","*");
-res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-return connect.execSQLQuery("delete from cliente where id="+ req.params.id, res);
-})
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    return connect.execSQLQuery("delete from cliente where id="+ req.params.id, res);
+    })
+
+//app.delete('/clientes/:id', (req, res) =>{
+//return connect.execSQLQuery('delete from cliente where id=' + parseInt(req.params.id), res);
+//})
 
 //port Number
 const PORT= process.env.PORT||5000;
